38ecc7780c544f5c51950d5f5c0e64465a64fab1
pierre villard <pvillard@ilki.fr>
2020-07-12 21:52:46 +0200

    Update setup-hosts.sh

--- setup-hosts.sh
+++ setup-hosts.sh
@@ -18,7 +18,7 @@
     echo "#$DISTRO#"
     if [[ $DISTRO == centos* ]]; then
         sudo killall -9 yum
-        sudo yum install python -y
+        sudo yum install python3 -y
         sudo yum install openssh-server -y
     elif [[ $DISTRO == ubuntu* ]] || [[ $DISTRO == debian* ]]; then
         export DEBIAN_FRONTEND=noninteractive
@@ -26,7 +26,7 @@
         sudo apt-get update
         sudo apt-get install -yqq software-properties-common
         sudo apt-get install -yqq openssh-server
-        sudo apt-get install -yqq python
+        sudo apt-get install -yqq python3
     else
         echo "Unsupported OS"
         exit
